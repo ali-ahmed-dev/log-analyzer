8c5d87c6a0414bf41a0cf1f6a1e2e431ac018e34
ali-ahmed-dev <aliahmedalzaidi493@gmail.com>
2026-07-22 03:42:10 +0300

    Add line counting functionality

--- Log Analyzer.py
+++ Log Analyzer.py
@@ -3,18 +3,27 @@
 
 
 def analyze_log(filename):
-    pass
+    line_count = 0
+    with open(filename, "r", encoding="utf-8") as file:
+        for _ in file:
+            line_count += 1
+    return line_count
 
 
-def generate_report():
-    pass
+def generate_report(filename, line_count):
+
+    print(f"""File        : {filename}
+Status      : Loaded Successfully
+Total Lines : {line_count}
+""")
 
 
 def main():
     print("Welcome to the Log Analyzer Tool")
-    filename = input("Enter the log file name: ")
     try:
-        analyze_log(filename)
+        filename = input("Enter the log file name: ")
+        line_count = analyze_log(filename)
+        generate_report(filename, line_count)
     except FileNotFoundError:
         print("File not found.")
 
